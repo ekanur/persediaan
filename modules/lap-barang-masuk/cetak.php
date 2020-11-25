90f619e1f03fe7173b1a06f8103816d61c9ad0ee
ichsanputr <githubichsan@gmail.com>
2020-11-25 19:04:10 +0700

    Lap Masuk PDF

--- modules/lap-barang-masuk/cetak.php
+++ modules/lap-barang-masuk/cetak.php
@@ -1,135 +1,54 @@
 <?php
-session_start();
-ob_start();
-
-// Panggil koneksi database.php untuk koneksi database
-require_once "../../config/database.php";
-// panggil fungsi untuk format tanggal
-include "../../config/fungsi_tanggal.php";
 
 $hari_ini = date("d-m-Y");
+$orgDate = $_GET['tgl_awal'];  
+$newDate = date("Y-m-d", strtotime($orgDate));  
+$orgDate1 = $_GET['tgl_akhir'];  
+$newDate1 = date("Y-m-d", strtotime($orgDate1));  
 
-// ambil data hasil submit dari form
-$tgl1     = $_GET['tgl_awal'];
-$explode  = explode('-',$tgl1);
-$tgl_awal = $explode[2]."-".$explode[1]."-".$explode[0];
-
-$tgl2      = $_GET['tgl_akhir'];
-$explode   = explode('-',$tgl2);
-$tgl_akhir = $explode[2]."-".$explode[1]."-".$explode[0];
 
 if (isset($_GET['tgl_awal'])) {
-    $no    = 1;
-    // fungsi query untuk menampilkan data dari tabel barang masuk
-    $query = mysqli_query($mysqli, "SELECT a.id_barang_masuk,a.tanggal_masuk,a.id_barang,a.jumlah_masuk,b.id_barang,b.nama_barang,b.id_satuan,c.id_satuan,c.nama_satuan
-                                    FROM is_barang_masuk as a INNER JOIN is_barang as b INNER JOIN is_satuan as c
-                                    ON a.id_barang=b.id_barang AND b.id_satuan=c.id_satuan
-                                    WHERE a.tanggal_masuk BETWEEN '$tgl_awal' AND '$tgl_akhir'
-                                    ORDER BY a.id_barang_masuk ASC") 
-                                    or die('Ada kesalahan pada query tampil Transaksi : '.mysqli_error($mysqli));
-    $count  = mysqli_num_rows($query);
-}
-?>
-<html xmlns="http://www.w3.org/1999/xhtml"> <!-- Bagian halaman HTML yang akan konvert -->
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-        <title>LAPORAN DATA BARANG MASUK</title>
-        <link rel="stylesheet" type="text/css" href="../../assets/css/laporan.css" />
-    </head>
-    <body>
-        <div id="title">
-            LAPORAN DATA BARANG MASUK 
-        </div>
-    <?php  
-    if ($tgl_awal==$tgl_akhir) { ?>
-        <div id="title-tanggal">
-            Tanggal <?php echo tgl_eng_to_ind($tgl1); ?>
-        </div>
-    <?php
-    } else { ?>
-        <div id="title-tanggal">
-            Tanggal <?php echo tgl_eng_to_ind($tgl1); ?> s.d. <?php echo tgl_eng_to_ind($tgl2); ?>
-        </div>
-    <?php
-    }
-    ?>
-        
-        <hr><br>
-        <div id="isi">
-            <table width="100%" border="0.3" cellpadding="0" cellspacing="0">
-                <thead style="background:#e8ecee">
-                    <tr class="tr-title">
-                        <th height="20" align="center" valign="middle">NO.</th>
-                        <th height="20" align="center" valign="middle">ID TRANSAKSI</th>
-                        <th height="20" align="center" valign="middle">TANGGAL</th>
-                        <th height="20" align="center" valign="middle">ID BARANG</th>
-                        <th height="20" align="center" valign="middle">NAMA BARANG</th>
-                        <th height="20" align="center" valign="middle">JUMLAH MASUK</th>
-                    </tr>
-                </thead>
-                <tbody>
-<?php
-    // jika data ada
-    if($count == 0) {
-        echo "  <tr>
-                    <td width='40' height='13' align='center' valign='middle'></td>
-                    <td width='120' height='13' align='center' valign='middle'></td>
-                    <td width='120' height='13' align='center' valign='middle'></td>
-                    <td width='80' height='13' align='center' valign='middle'></td>
-                    <td style='padding-left:5px;' width='210' height='13' valign='middle'></td>
-                    <td style='padding-left:5px;' width='100' height='13' valign='middle'></td>
-                </tr>";
-    }
-    // jika data tidak ada
-    else {
-        // tampilkan data
-        while ($data = mysqli_fetch_assoc($query)) {
-            $tanggal       = $data['tanggal_masuk'];
-            $exp           = explode('-',$tanggal);
-            $tanggal_masuk = tgl_eng_to_ind($exp[2]."-".$exp[1]."-".$exp[0]);
+    
+// memanggil library FPDF
+require('/opt/lampp/htdocs/persediaan/fpdf182/fpdf.php');
+// intance object dan memberikan pengaturan halaman PDF
+$pdf = new FPDF('l','mm','A5');
+// membuat halaman baru
+$pdf->AddPage();
+// setting jenis font yang akan digunakan
+$pdf->SetFont('Arial','B',20);
+$pdf->Cell(190,7,'Laporan Barang Masuk',0,4,'C');
 
-            // menampilkan isi tabel dari database ke tabel di aplikasi
-            echo "  <tr>
-                        <td width='40' height='13' align='center' valign='middle'>$no</td>
-                        <td width='120' height='13' align='center' valign='middle'>$data[id_barang_masuk]</td>
-                        <td width='120' height='13' align='center' valign='middle'>$tanggal_masuk</td>
-                        <td width='80' height='13' align='center' valign='middle'>$data[id_barang]</td>
-                        <td style='padding-left:5px;' width='210' height='13' valign='middle'>$data[nama_barang]</td>
-                        <td style='padding-left:5px;' width='100' height='13' valign='middle'>$data[jumlah_masuk] $data[nama_satuan]</td>
-                    </tr>";
-            $no++;
-        }
-    }
-?>	
-                </tbody>
-            </table>
+// Memberikan space kebawah agar tidak terlalu rapat
+$pdf->Cell(10,7,'',0,1);
 
-            <div id="footer-tanggal">
-                Bandarlampung, <?php echo tgl_eng_to_ind("$hari_ini"); ?>
-            </div>
-            <div id="footer-jabatan">
-                Pimpinan
-            </div>
-            
-            <div id="footer-nama">
-                Indra Setyawantoro, S.Kom.
-            </div>
-        </div>
-    </body>
-</html><!-- Akhir halaman HTML yang akan di konvert -->
-<?php
-$filename="LAPORAN DATA BARANG MASUK.pdf"; //ubah untuk menentukan nama file pdf yang dihasilkan nantinya
-//==========================================================================================================
-$content = ob_get_clean();
-$content = '<page style="font-family: freeserif">'.($content).'</page>';
-// panggil library html2pdf
-require_once('../../assets/plugins/html2pdf_v4.03/html2pdf.class.php');
-try
-{
-    $html2pdf = new HTML2PDF('P','F4','en', false, 'ISO-8859-15',array(10, 10, 10, 10));
-    $html2pdf->setDefaultFont('Arial');
-    $html2pdf->writeHTML($content, isset($_GET['vuehtml']));
-    $html2pdf->Output($filename);
+
+$pdf->SetFont('Arial','B',10);
+$pdf->SetFillColor(0,0,0);
+$pdf->Cell(10,6,'NO',1,0);
+$pdf->Cell(40,6,'ID BARANG MASUK',1,0);
+$pdf->Cell(60,6,'BARANG',1,0);
+$pdf->Cell(35,6,'JUMLAH MASUK',1,0);
+$pdf->Cell(45,6,'TANGGAL MASUK',1,1);
+
+
+$pdf->SetFont('Arial','',10);
+$no = 1;
+include '/opt/lampp/htdocs/persediaan/config/database.php';
+$mahasiswa = mysqli_query($mysqli, "SELECT *,is_barang.nama_barang FROM `is_barang_masuk` INNER JOIN is_barang ON is_barang_masuk.id_barang = is_barang.id_barang WHERE tanggal_masuk BETWEEN '".$newDate."' AND '".$newDate1."'");
+while ($row = mysqli_fetch_array($mahasiswa)){
+    $pdf->Cell(10,6,$no++,1,0);
+    $pdf->Cell(40,6,$row['id_barang_masuk'],1,0);
+    $pdf->Cell(60,6,$row['nama_barang'],1,0);
+    $pdf->Cell(35,6,$row['jumlah_masuk'],1,0);
+    $pdf->Cell(45,6,$row['tanggal_masuk'],1,1);
+
+
 }
-catch(HTML2PDF_exception $e) { echo $e; }
+
+$pdf->Output();
+
+}else{
+    
+}
 ?>
